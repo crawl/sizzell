500ef2736bdb86fb2f8a79533b864b9e54ea5ba7
Darshan Shaligram <scintilla@gmail.com>
2010-11-13 15:56:55 +0530

    Suppress death milestones (kilobyte)

--- gretell.pl
+++ gretell.pl
@@ -139,8 +139,7 @@ sub newsworthy
   my $br_enter = $type eq 'enter' || $type eq 'br.enter';
   my $place_branch = game_place_branch($g);
 
-  return 0
-    if ($type eq 'crash' || $type eq 'monstrous');
+  return 0 if grep($_ eq $type, 'crash', 'monstrous', 'death');
 
   return 0
     if $br_enter
